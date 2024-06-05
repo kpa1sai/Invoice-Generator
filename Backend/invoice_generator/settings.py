7d262d33766787f922de8c60a3d126a9de5ad116
Korlapati Pavan Sai <korlapatipavansai@gmail.com>
2024-06-04 21:38:42 -0500

    Update readme file

--- Backend/invoice_generator/settings.py
+++ Backend/invoice_generator/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'invoices',
 ]
 
 MIDDLEWARE = [
@@ -47,9 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'invoice_generator.urls'
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Invoice-Generator
+
+## Backend
+
+Run these commands in the backend terminal:
+
+````python -m venv env
+```env/Scripts/activate
+```pip install -r requirements.txt
+```python manage.py makemigrations
+```python manage.py migrate
+```python manage.py runserver
+````
